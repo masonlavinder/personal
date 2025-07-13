a415d36e321ac8129e48dc4efd927c3579b632f4
Mason <mlavinder@mpr.com>
2025-07-13 13:09:51 -0400

    inital upload

---
+++ src/pages/Home.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+import '../styles/Home.css';
+
+// const postPath = `${import.meta.env.BASE_URL || '/'}posts`.replace('//', '/');
+
+const Home: React.FC = () => {
+//   const navigate = useNavigate();
+
+//   const goToAbout = () => {
+//     navigate('/about');
+//   };
+
+  return (
+    <div className="home-wrapper">
+        <p>test</p>
+    </div>
+  );
+};
+
+export default Home;
