a3aa93ef4dfaa6cfb50811ef5b9d4d58d6fb39e8
masonlavinder <mlavinder@mpr.com>
2025-12-14 12:19:34 -0500

    update for css

--- src/pages/LinkList/linksData.ts
+++ src/pages/LinkList/linksData.ts
@@ -6,76 +6,76 @@ export const sampleLinks: Link[] = [
     title: 'Coolors',
     url: 'https://coolors.co/',
     description: 'Fast color palette generator for creating solid color schemes.',
-    categories: ['Design', 'Tools']
+    categories: ['design', 'tools']
   },
   {
     id: '2',
     title: '7 Rules for Creating Gorgeous UI',
     url: 'https://medium.com/@erikdkennedy/7-rules-for-creating-gorgeous-ui-part-1-559d4e805cda',
     description: 'Essential design principles for developers who want to get started.',
-    categories: ['Design', 'Learning']
+    categories: ['design', 'learning']
   },
   {
     id: '3',
     title: 'Figma',
     url: 'https://figma.com',
     description: 'Industry Standard. Stop mocking in PowerPoint',
-    categories: ['Design', 'Tools']
+    categories: ['design', 'tools']
   },
   {
     id: '4',
     title: 'Software is Joy',
     url: 'https://blog.jsbarretto.com/post/software-is-joy?utm_source=tldrwebdev',
-    description: 'Thoughtful article about the joy and craft of software development.',
-    categories: ['Learning', 'Philosophy']
+    description: 'Thoughtful article about the joy and craft of software dev.',
+    categories: ['learning', 'articles']
   },
   {
     id: '5',
     title: 'TLDR Tech',
     url: 'https://tldr.tech/',
     description: 'This actually does help me stay up to date on news. Despite their Instagram ads.',
-    categories: ['Learning', 'News']
+    categories: ['learning', 'news']
   },
   {
     id: '6',
     title: 'Vite',
     url: 'https://vite.dev/',
     description: 'My default frontend build tool with extremely-fast dev server. Scales well.',
-    categories: ['Development', 'Tools']
+    categories: ['dev', 'tools']
   },
   {
     id: '7',
     title: 'CSS Loaders',
     url: 'https://css-loaders.com/',
     description: 'Collection of copy-paste CSS loading animations.',
-    categories: ['Design', 'CSS']
+    categories: ['design']
   },
   {
     id: '8',
     title: 'Google Fonts',
     url: 'https://fonts.google.com/',
     description: 'Free, open-source fonts optimized for the web.',
-    categories: ['Design', 'Typography']
+    categories: ['design']
   },
   {
     id: '9',
     title: 'Storybook',
     url: 'https://storybook.js.org/',
     description: 'Tool for building UI components in isolation for React et al.',
-    categories: ['Development', 'Tools']
+    categories: ['dev', 'tools']
   },
   {
     id: '10',
     title: 'Lucide Icons',
     url: 'https://lucide.dev/guide/packages/lucide-react',
     description: 'Consistent icon library for React applications.',
-    categories: ['Design', 'Development']
+    categories: ['design', 'dev']
   },
   {
     id: '11',
     title: 'Visual Studio Code',
     url: 'https://code.visualstudio.com/',
     description: 'I am basic. I like VSCode.',
-    categories: ['Development', 'Tools']
+    categories: ['dev', 'tools']
   }
 ];
